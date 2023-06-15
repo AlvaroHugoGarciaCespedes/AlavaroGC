ef7641a192ef561545c44258a98750ca3cb5fa18
raulDeDevWorld <proyection009@gmail.com>
2023-06-15 00:19:53 -0400

    DC

--- src/components/Navleft.jsx
+++ src/components/Navleft.jsx
@@ -33,9 +33,13 @@ export default function Navbar({ children, funcion }) {
     const handlerAdd = () => {
         setAdd(!add)
     }
+
+    const handlerShare = () => {
+        window.open('https://wa.me/?text=https://alvarogc.com/', '_blank')
+    }
     return (
         <>
-           {user &&  <div className={`fixed right-[20px] transition-all ease-in-out duration-300 ${add ? 'bottom-[150px] lg:bottom-[73px]' : 'bottom-[-200px] lg:bottom-[73px]'} z-40`}>
+            {user && <div className={`fixed right-[20px] transition-all ease-in-out duration-300 ${add ? 'bottom-[150px] lg:bottom-[73px]' : 'bottom-[-200px] lg:bottom-[73px]'} z-40`}>
                 <button type="button" onClick={() => funcion('Portada')} className="h-[50px] w-[50px] my-1 text-blue-700 bg-gray-950 border border-gray-700 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-gray-500 font-medium rounded-full text-sm p-2.5 text-center flex justify-center items-center ">
                     <svg className="w-6 h-6 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" fill="#636363"></path>
@@ -80,12 +84,18 @@ export default function Navbar({ children, funcion }) {
                         </svg>
                         <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 pt-[6px] pb-[7px] dark:group-hover:text-blue-500">Articulos</span>
                     </a>
-                    <button className="inline-flex flex-col items-center justify-end px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={handlerAdd}>
+                    {user ? <button className="inline-flex flex-col items-center justify-end px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={handlerAdd}>
                         <svg className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" fill="#636363"></path>
                         </svg>
                         <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 pb-[7px] dark:group-hover:text-blue-500">Add</span>
                     </button>
+                    : <button className="inline-flex flex-col items-center justify-end px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={handlerShare}>
+                        <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path d="M15 20C14.1667 20 13.4583 19.7083 12.875 19.125C12.2917 18.5417 12 17.8333 12 17C12 16.8833 12.0083 16.7623 12.025 16.637C12.0417 16.5117 12.0667 16.3993 12.1 16.3L5.05 12.2C4.76667 12.45 4.45 12.646 4.1 12.788C3.75 12.93 3.38333 13.0007 3 13C2.16667 13 1.45833 12.7083 0.875 12.125C0.291667 11.5417 0 10.8333 0 10C0 9.16667 0.291667 8.45833 0.875 7.875C1.45833 7.29167 2.16667 7 3 7C3.38333 7 3.75 7.071 4.1 7.213C4.45 7.355 4.76667 7.55067 5.05 7.8L12.1 3.7C12.0667 3.6 12.0417 3.48767 12.025 3.363C12.0083 3.23833 12 3.11733 12 3C12 2.16667 12.2917 1.45833 12.875 0.875C13.4583 0.291667 14.1667 0 15 0C15.8333 0 16.5417 0.291667 17.125 0.875C17.7083 1.45833 18 2.16667 18 3C18 3.83333 17.7083 4.54167 17.125 5.125C16.5417 5.70833 15.8333 6 15 6C14.6167 6 14.25 5.92933 13.9 5.788C13.55 5.64667 13.2333 5.45067 12.95 5.2L5.9 9.3C5.93333 9.4 5.95833 9.51267 5.975 9.638C5.99167 9.76333 6 9.884 6 10C6 10.1167 5.99167 10.2377 5.975 10.363C5.95833 10.4883 5.93333 10.6007 5.9 10.7L12.95 14.8C13.2333 14.55 13.55 14.3543 13.9 14.213C14.25 14.0717 14.6167 14.0007 15 14C15.8333 14 16.5417 14.2917 17.125 14.875C17.7083 15.4583 18 16.1667 18 17C18 17.8333 17.7083 18.5417 17.125 19.125C16.5417 19.7083 15.8333 20 15 20Z" fill="#636363" />
+                        </svg>
+                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 pb-[7px] dark:group-hover:text-blue-500">Share</span>
+                    </button>}
                 </div>
             </div>
         </>
