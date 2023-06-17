797283331569cc8385163d1b106272c4cb7cea0f
raulDeDevWorld <proyection009@gmail.com>
2023-06-16 21:18:27 -0400

    DC

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -173,7 +173,8 @@ function close () {
                                 <input type="text" name="twiter" className="block w-full rounded-md border-0 p-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={data && data.frontPage && data.frontPage['twiter']} />
                             </div>
                             <div className="sm:col-span-3">
-                                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">Gmail</label>
+                                <label htmlFor="first-name" className="block 
+                                text-sm font-medium leading-6 text-gray-900">Gmail</label>
                                 <input type="text" name="gmail" className="block w-full rounded-md border-0 p-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={data && data.frontPage && data.frontPage['google']} />
                             </div>
                             <div className="sm:col-span-3">
