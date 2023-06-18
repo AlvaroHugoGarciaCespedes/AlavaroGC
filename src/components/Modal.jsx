9e4c537ec0ebbdb1694934356cdcb5b668ee3965
raulDeDevWorld <proyection009@gmail.com>
2023-06-18 09:47:41 -0400

    DC

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -166,7 +166,7 @@ export default function Modal({ theme, styled, click, children, }) {
                             </div>
                             <div className="sm:col-span-3">
                                 <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">Whatsapp</label>
-                                <input type="text" name="whatsapp" className="block w-full rounded-md border-0 p-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={data && data.frontPage && data.frontPage['whatsapp']} ref={inputRefWhatsApp} />
+                                <input type="text" name="whatsapp" className="block w-full rounded-md border-0 p-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={data && data.frontPage && data.frontPage['whatsapp']} />
                             </div>
                             <div className="sm:col-span-3">
                                 <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">Facebook</label>
