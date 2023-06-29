889eb4cab6566bce71a42eaf2998add3af3ad29f
raulDeDevWorld <proyection009@gmail.com>
2023-06-29 02:07:25 -0400

    DC

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -47,7 +47,7 @@ function Home() {
     userDB && <main className="flex min-h-screen w-full flex-col items-center justify-between px-5">
       {userDB && <Modal theme={modal} i={item} />}
 
-      <section className='min-h-screen  w-full pt-18 pb-0 flex flex-col justify-center items-center lg:flex-row items-center '>
+      <section className='min-h-screen  w-full lg:pt-[70px] pb-0 flex flex-col justify-center items-center lg:flex-row items-center '>
         <div className='w-screen h-[40vh] flex justify-center items-end lg:w-[50vw] lg:h-[100vh]' style={{ backgroundImage: `url(${userDB.frontPage['url']})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
         </div>
         {/* <img src={userDB.frontPage['url']} className='w-screen h-[35vh] flex justify-center items-end lg:w-[50vw] lg:h-[100vh]' alt="" /> */}
@@ -70,7 +70,7 @@ function Home() {
             <div className='flex flex-col'><span className='text-[#00A582] text-[16px]'>Celeridad</span></div>
           </div>
           <div className=' lg:grid lg:grid-cols-2 lg:gap-5 '>
-            <Link href="#Servicios"><Button theme="Secondary" >Servicios</Button></Link> 
+            <Link href="#Servicios"><Button theme="Secondary" >Servicios</Button></Link>
             <Link href={`https://api.whatsapp.com/send?phone=${userDB.frontPage['whatsapp']}&text=Hola%20Alvaro`}><Button theme="Primary" >Contactar</Button></Link>
           </div>
         </div>
